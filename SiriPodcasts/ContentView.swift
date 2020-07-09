f1a07bdfa3439fe6b341040dc6ee833619dbece0
Pranav Kasetti <pranavkasetti@gmail.com>
2020-07-09 03:28:42 +0100

    Initial Commit

---
+++ SiriPodcasts/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SiriPodcasts
+//
+//  Created by Pranav Kasetti on 09/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SiriPodcasts/SiriPodcastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriPodcastsApp.swift
+//  SiriPodcasts
+//
+//  Created by Pranav Kasetti on 09/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SiriPodcastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
